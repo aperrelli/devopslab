425fbaa21e78a31045eb4df75d3dfa090d60a889
Anderson Perrelli <anderson.perrelli@liferay.com>
2022-03-01 17:21:22 -0300

    Test Fail Pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - Anderson Perrelli"
+    return "Hello World - Anderson"
 
 if __name__ == '__main__':
     app.run()
