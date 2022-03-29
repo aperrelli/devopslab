45cfd8700677905e690da63f43b4dbd736ad7538
Anderson Perrelli <anderson.perrelli@liferay.com>
2022-03-29 20:54:39 -0300

    Change app

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - Anderson Perrelli"
+    return "Hello World - Anderson Perrelli Deploy"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
